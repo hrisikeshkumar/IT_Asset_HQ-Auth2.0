ea37f3b6d4ede490b267408d35104a33d2e5111e
hrisikeshkumar <hrisikeshkumar@gmail.com>
2024-09-11 20:49:30 +0530

    ok

--- Program.cs
+++ Program.cs
@@ -24,17 +24,21 @@ var initialScopes =new string[] { Constants1.ScopeUserRead, Constants1.ScopeGrou
 
 CacheSettings cacheSettings = new CacheSettings
 {
-    SlidingExpirationInSeconds = builder.Configuration.GetValue<string>("CacheSettings:SlidingExpirationInSeconds"),
-    AbsoluteExpirationInSeconds = builder.Configuration.GetValue<string>("CacheSettings:AbsoluteExpirationInSeconds")
+    SlidingExpirationInSeconds = builder.Configuration
+                                .GetValue<string>("CacheSettings:SlidingExpirationInSeconds"),
+    AbsoluteExpirationInSeconds = builder.Configuration
+                                .GetValue<string>("CacheSettings:AbsoluteExpirationInSeconds")
 };
 
 builder.Services.Configure<CookiePolicyOptions>(options =>
 {
+    
     // This lambda determines whether user consent for non-essential cookies is needed for a given request.
     options.CheckConsentNeeded = context => true;
     options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
     // Handling SameSite cookie according to https://docs.microsoft.com/en-us/aspnet/core/security/samesite
     options.HandleSameSiteCookieCompatibility();
+
 });
 
 // This is required to be instantiated before the OpenIdConnectOptions starts getting configured.
@@ -46,39 +50,39 @@ JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
 // Adds Microsoft Identity platform (AAD v2.0) support to protect this Api
 builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApp(options =>
-    {
-        // Ensure default token validation is carried out
-        builder.Configuration.Bind("AzureAd", options);
-
-        // The following lines code instruct the asp.net core middleware to use the data in the "roles" claim in the [Authorize] attribute, policy.RequireRole() and User.IsInRole()
-        // See https://docs.microsoft.com/aspnet/core/security/authorization/roles for more info.
-        options.TokenValidationParameters.RoleClaimType = "groups";
-
-        /// <summary>
-        /// Below you can do extended token validation and check for additional claims, such as:
-        ///
-        /// - check if the caller's tenant is in the allowed tenants list via the 'tid' claim (for multi-tenant applications)
-        /// - check if the caller's account is homed or guest via the 'acct' optional claim
-        /// - check if the caller belongs to right roles or groups via the 'roles' or 'groups' claim, respectively
-        ///
-        /// Bear in mind that you can do any of the above checks within the individual routes and/or controllers as well.
-        /// For more information, visit: https://docs.microsoft.com/azure/active-directory/develop/access-tokens#validate-the-user-has-permission-to-access-this-data
-        /// </summary>
-
-        options.Events.OnTokenValidated = async context =>
         {
-            if (context != null)
+            // Ensure default token validation is carried out
+            builder.Configuration.Bind("AzureAd", options);
+
+            // The following lines code instruct the asp.net core middleware to use the data in the "roles" claim in the [Authorize] attribute, policy.RequireRole() and User.IsInRole()
+            // See https://docs.microsoft.com/aspnet/core/security/authorization/roles for more info.
+            options.TokenValidationParameters.RoleClaimType = "groups";
+
+            /// <summary>
+            /// Below you can do extended token validation and check for additional claims, such as:
+            ///
+            /// - check if the caller's tenant is in the allowed tenants list via the 'tid' claim (for multi-tenant applications)
+            /// - check if the caller's account is homed or guest via the 'acct' optional claim
+            /// - check if the caller belongs to right roles or groups via the 'roles' or 'groups' claim, respectively
+            ///
+            /// Bear in mind that you can do any of the above checks within the individual routes and/or controllers as well.
+            /// For more information, visit: https://docs.microsoft.com/azure/active-directory/develop/access-tokens#validate-the-user-has-permission-to-access-this-data
+            /// </summary>
+
+            options.Events.OnTokenValidated = async context =>
             {
-                List<string> requiredGroupsIds = builder.Configuration.GetSection("AzureAd:Groups")
-                                .AsEnumerable().Select(x => x.Value).Where(x => x != null).ToList();
+                if (context != null)
+                {
+                    List<string> requiredGroupsIds = builder.Configuration.GetSection("AzureAd:Groups")
+                                    .AsEnumerable().Select(x => x.Value).Where(x => x != null).ToList();
 
-                // Calls method to process groups overage claim (before policy checks kick-in)
-                await GraphHelper.ProcessAnyGroupsOverage(context, requiredGroupsIds, cacheSettings);
-            }
+                    // Calls method to process groups overage claim (before policy checks kick-in)
+                    await GraphHelper.ProcessAnyGroupsOverage(context, requiredGroupsIds, cacheSettings);
+                }
 
-            await Task.CompletedTask;
-        };
-    })
+                await Task.CompletedTask;
+            };
+        })
     .EnableTokenAcquisitionToCallDownstreamApi(options => builder.Configuration.Bind("AzureAd", options), initialScopes)
     .AddMicrosoftGraph(builder.Configuration.GetSection("GraphAPI"))
     .AddInMemoryTokenCaches();
@@ -86,10 +90,8 @@ builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
 // Adding authorization policies that enforce authorization using Azure AD security groups.
 builder.Services.AddAuthorization(options =>
 {
-   
     // this policy stipulates that users in Chapter Office can access resources
     options.AddPolicy(AuthorizationPolicies.Chapter, policy => policy.RequireRole(builder.Configuration["Groups:Chapter"], builder.Configuration["Groups:ITHardwareManager"]));
-
 
     // this policy stipulates that users in ROs Office can access resources
     options.AddPolicy(AuthorizationPolicies.ROsGroup, policy => policy.RequireRole( builder.Configuration["Groups:ROs"], builder.Configuration["Groups:ITHardwareManager"]));
@@ -102,7 +104,6 @@ builder.Services.AddAuthorization(options =>
 
     // this policy stipulates that users in IT Hardware Staffs can access resources
     options.AddPolicy(AuthorizationPolicies.ITHardwareManager, policy => policy.RequireRole( builder.Configuration["Groups:ITHardwareManager"]));
-
 
 });
 
