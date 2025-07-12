21e117c802d6f2dbb88aecc9bec2a3f6c415a2c3
hrisikeshkumar <hrisikeshkumar@gmail.com>
2025-07-12 18:37:48 +0530

    ok

--- Areas/Admin/Controllers/QRCodeController.cs
+++ Areas/Admin/Controllers/QRCodeController.cs
@@ -1,9 +1,17 @@
 ﻿using IT_Hardware.Areas.Admin.Data;
 using IT_Hardware.Areas.Admin.Models;
 using Microsoft.AspNetCore.Mvc;
+using IT_Hardware.Infra;
+using Microsoft.AspNetCore.Authorization;
+using QRCoder;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 
 namespace IT_Hardware.Areas.Admin.Controllers
 {
+    [Authorize(Policy = AuthorizationPolicies.ITSupportEngineers)]
+    [Area("Admin")]
     public class QRCodeController : Controller
     {
         public IActionResult AssetsInfo()
@@ -17,7 +25,28 @@ namespace IT_Hardware.Areas.Admin.Controllers
 
         public IActionResult CodeGeneration()
         {
-            return View();
+
+            var qrCodes = new Dictionary<string, string>();
+            var qrGenerator = new QRCodeGenerator();
+
+            List<string> qrCodeInputs = new List<string>
+            {
+                "https://example.com/asset1",
+                "https://example.com/asset2",
+                "https://example.com/asset3"
+            };
+
+            foreach (var input in qrCodeInputs)
+            {
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(input, QRCodeGenerator.ECCLevel.Q);
+                var pngQrCode = new PngByteQRCode(qrCodeData);
+                byte[] qrCodeBytes = pngQrCode.GetGraphic(20);
+                var base64 = Convert.ToBase64String(qrCodeBytes);
+                qrCodes.Add(input, $"data:image/png;base64,{base64}");
+            }
+
+            return View(qrCodes);
+
         }
 
         public IActionResult Asset_Detail_Information()
